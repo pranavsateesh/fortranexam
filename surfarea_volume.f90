e033336a9f39d8f295131e0147878d036cdef92d
pranavsateesh <ps905@snu.edu.in>
2020-12-12 01:47:41 +0530

    solved for volume

--- surfarea_volume.f90
+++ surfarea_volume.f90
@@ -4,20 +4,26 @@ program surfvol
     rad = 2
     pi = 3.1415
     saa = 4.0*pi*(rad**2)
-    vol = 4.0*pi*(rad**3) 
-    do i=1,10000
-    xrand = 2+4*rand()
-    yrand = 2+4*rand()
-    zrand = 2+4*rand()
-    radius = (x**2) + (y**2) + (z**2)
-    if (radius .LE. rad**2) then 
+    vol = 4.0/3.0*pi*(rad**3) 
+    count = 0 
+    count2 = 0
+    error = 0.5
+    do i=1,1000000
+    xrand = 0+2*rand()
+    yrand = 0+2*rand()
+    zrand = 0+2*rand()
+    radius = sqrt((xrand**2)+(yrand**2)+(zrand**2))
+    if (radius .LE. rad) then 
     count = count +1
-    if (radius .EQ. rad**2) then 
-        count2 = count2 +1
     end if
-    end if    
+    if (radius .LE. rad+error) then 
+        if (radius .GE. rad-error) then
+            count2 = count2 +1
+        end if
+    end if     
 end do
-    estimate2=((6*rad**2)*count)/10000 
-    estimate1=((2*rad**3)*count)/10000
-    print*,estimate1,estimate1,saa,vol
+    print*,count2
+    estimate2=(4*(rad**2)*count2)/1000000 
+    estimate1=(8*(rad**3)*count)/1000000
+    print*,estimate1,estimate2,saa,vol
 end program
